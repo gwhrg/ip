b3b5ab9ca2f15f59ece34b718d4e44ab4e0b0440
gwhrg <203697007+gwhrg@users.noreply.github.com>
2026-01-28 20:18:08 +0800

    Implement Level 1 Echo functionality

--- src/main/java/Kraken.java
+++ src/main/java/Kraken.java
@@ -1,11 +1,29 @@
+import java.util.Scanner;
+
 public class Kraken {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Kraken");
-        System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.println("____________________________________________________________");
+            System.out.println(" Hello! I'm Kraken");
+            System.out.println(" What can I do for you?");
+            System.out.println("____________________________________________________________");
+
+            while (in.hasNextLine()) {
+                String userInput = in.nextLine();
+                String trimmedInput = userInput.trim();
+
+                if (trimmedInput.equals("bye")) {
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" Bye. Hope to see you again soon!");
+                    System.out.println("____________________________________________________________");
+                    return;
+                }
+
+                System.out.println("____________________________________________________________");
+                System.out.println(" " + userInput);
+                System.out.println("____________________________________________________________");
+            }
+        }
     }
 }
 
