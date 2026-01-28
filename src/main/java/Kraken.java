3878a2363e1119eb2d472f5d260eaec041a2bf3a
gwhrg <203697007+gwhrg@users.noreply.github.com>
2026-01-28 20:22:18 +0800

    Add task storage and list functionality

--- src/main/java/Kraken.java
+++ src/main/java/Kraken.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class Kraken {
     public static void main(String[] args) {
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         try (Scanner in = new Scanner(System.in)) {
             System.out.println("____________________________________________________________");
             System.out.println(" Hello! I'm Kraken");
@@ -17,11 +20,19 @@ public class Kraken {
                     System.out.println(" Bye. Hope to see you again soon!");
                     System.out.println("____________________________________________________________");
                     return;
+                } else if (trimmedInput.equals("list")) {
+                    System.out.println("____________________________________________________________");
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    }
+                    System.out.println("____________________________________________________________");
+                } else {
+                    tasks[taskCount] = trimmedInput;
+                    taskCount++;
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" added: " + trimmedInput);
+                    System.out.println("____________________________________________________________");
                 }
-
-                System.out.println("____________________________________________________________");
-                System.out.println(" " + userInput);
-                System.out.println("____________________________________________________________");
             }
         }
     }
